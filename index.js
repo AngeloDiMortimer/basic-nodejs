9254ea10a4ad91b890245e132566ce859abcc93b
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-09-29 17:24:44 -0400

    Rewritten with express

--- index.js
+++ index.js
@@ -1,54 +1,13 @@
-const http = require('http');
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'public', req.url === '/' ?
-    'index.html' : req.url);
+const app = express();
 
-    let extname = path.extname(filePath);
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-    let contentType = 'text/html';
-
-    switch(extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if(err) {
-            if(err.code == 'ENOENT') {
-                //Page not found
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(200, {'Content-Type' : 'text/html'})
-                    res.end(content, 'utf8');
-                })
-            } else {
-                //Some server error
-                res.writeHead(500);
-                res.end(content, 'utf8');
-            }
-        } else {
-            //Success
-            res.writeHead(200, {'Content-Type': contentType});
-            res.end(content, 'utf8');
-        }
-    })
-})
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', '404.html')));
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
